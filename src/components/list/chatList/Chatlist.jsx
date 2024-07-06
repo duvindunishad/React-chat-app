e36201aef6e88e11785215714b6ba267ec10948e
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-06 16:42:16 +0530

    create chat list and style it

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react"
 import "./Chatlist.css"
 
 const Chatlist = () => {
+  const [addMode,setAddMode]= useState(false)
   return (
     <div className="chatList">
-      Chatlist
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search"/>
+        </div>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add"
+        onClick={()=> setAddMode((prev) => !prev)}/>
+      </div>
+
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Nishad</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Nishad</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Nishad</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Nishad</span>
+          <p>hello</p>
+        </div>
+      </div>
     </div>
   )
 }
