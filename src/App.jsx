957d82b3a4f0ed4c45f38c218f8ad5ba62472c77
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-07 12:13:12 +0530

    create login and registration front end

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import Chat from './components/chat/Chat'
 import Details from './components/details/Details'
 import List from './components/list/List'
+import Login from './components/login/Login'
 
 function App() {
+
+  const user =false
   
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Details/>
+      {user ? (
+        <>
+        <List/>
+        <Chat/>
+        <Details/>
+        </>
+      ) : (<Login/>)}
     </div>
   )
 }
